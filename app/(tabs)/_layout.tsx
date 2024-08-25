a6ad89e477e621cbcb4c37c4d2eda32a3d24d1ca
Anton Bochkovskiy <unundertale1@ukr.net>
2024-08-25 15:34:33 +0300

    update tabs configuration

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,33 @@
+import { Colors } from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'All' }} />
-      <Tabs.Screen name="recent" options={{ title: 'Recent' }} />
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary[500] },
+        headerTintColor: '#fff',
+        tabBarStyle: { backgroundColor: Colors.primary[500] },
+        tabBarActiveTintColor: Colors.accent[500],
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'All',
+          headerTitle: 'All Expenses',
+          tabBarIcon: (props) => <Ionicons name="hourglass" {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="recent"
+        options={{
+          title: 'Recent',
+          headerTitle: 'Recent Expenses',
+          tabBarIcon: (props) => <Ionicons name="calendar" {...props} />,
+        }}
+      />
     </Tabs>
   )
 }
