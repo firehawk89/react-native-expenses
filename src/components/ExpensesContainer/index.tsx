127fba60e225a9839b850d136b6952d87fdcdff2
Anton Bochkovskiy <unundertale1@ukr.net>
2024-08-25 15:31:41 +0300

    add expenses container component

---
+++ src/components/ExpensesContainer/index.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+import { View } from 'react-native'
+
+import ExpensesList from './components/ExpensesList'
+import ExpensesSummary from './components/ExpensesSummary'
+import { Expense, PeriodType } from './types'
+
+// FIXME: Replace with real data
+const EXPENSES: Expense[] = [
+  {
+    id: '1',
+    title: 'Coffee',
+    amount: 3.5,
+    date: new Date(),
+  },
+  {
+    id: '2',
+    title: 'Groceries',
+    amount: 50,
+    date: new Date(),
+  },
+  {
+    id: '3',
+    title: 'Lunch',
+    amount: 12,
+    date: new Date(),
+  },
+]
+
+interface ExpensesContainerProps {
+  expenses: Expense[]
+  period: PeriodType
+}
+
+const ExpensesContainer: FC<ExpensesContainerProps> = ({
+  expenses,
+  period,
+}) => {
+  console.log('ExpensesContainer', { expenses })
+  return (
+    <View>
+      <ExpensesSummary data={EXPENSES} period={period} />
+      <ExpensesList data={EXPENSES} />
+    </View>
+  )
+}
+
+export default ExpensesContainer
