a5b762eb23c18943a00703fdedfd6730be442abf
Anton Bochkovskiy <unundertale1@ukr.net>
2024-08-25 12:55:59 +0300

    add eslint autosort configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'perfectionist'],
   rules: {
     'prettier/prettier': 'error',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+      },
+    ],
   },
 }
