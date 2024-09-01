6ce203bdd78351ca9d6295b3201e82f22671d852
Anton Bochkovskiy <unundertale1@ukr.net>
2024-09-01 09:50:34 +0300

    add ManageExpense screen

---
+++ src/screens/ManageExpense.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const ManageExpense: FC = () => {
+  return (
+    <View>
+      <Text>Manage Expense</Text>
+    </View>
+  )
+}
+
+export default ManageExpense
