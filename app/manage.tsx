515a80e840b528a8df706f5e765334cdebb4bfa5
Anton Bochkovskiy <unundertale1@ukr.net>
2024-09-01 09:50:58 +0300

    use manage screen component for UI rendering

--- app/manage.tsx
+++ app/manage.tsx
@@ -1,17 +1,5 @@
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import ManageExpense from '@/screens/ManageExpense'
 
-export default function ManageExpense() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Manage Expense</Text>
-      <Link href="/(tabs)">Back to All Expenses</Link>
-    </View>
-  )
+export default function Manage() {
+  return <ManageExpense />
 }
