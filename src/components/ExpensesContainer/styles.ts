537e076d0ac0c5e0cc0a508a8f49f369d9acfe70
Anton Bochkovskiy <unundertale1@ukr.net>
2024-09-07 21:32:52 +0300

    add expenses container styles

---
+++ src/components/ExpensesContainer/styles.ts
@@ -0,0 +1,12 @@
+import { Colors } from '@/utils/Colors'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: Colors.primary[700],
+  },
+})
+
+export default styles
