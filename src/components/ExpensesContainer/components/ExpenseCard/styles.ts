0a982ae098dd04aa4a7d9a47e71a55a8c8e97953
Anton Bochkovskiy <unundertale1@ukr.net>
2024-09-07 21:35:20 +0300

    add expense card component styles

---
+++ src/components/ExpensesContainer/components/ExpenseCard/styles.ts
@@ -0,0 +1,38 @@
+import { Colors } from '@/utils/Colors'
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 8,
+    padding: 24,
+    borderRadius: 6,
+    elevation: 3,
+    shadowRadius: 4,
+    shadowColor: Colors.gray[500],
+    shadowOpacity: 0.4,
+    shadowOffset: { width: 1, height: 1 },
+    backgroundColor: Colors.primary[500],
+  },
+  text: {
+    color: Colors.primary[50],
+  },
+  title: {
+    marginBottom: 4,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  amountContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 4,
+    backgroundColor: 'white',
+  },
+  amount: {
+    fontWeight: 'bold',
+    color: Colors.primary[500],
+  },
+})
