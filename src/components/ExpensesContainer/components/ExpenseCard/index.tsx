6a7aec3b9faa1cfc4079c3808f8acfbfd2554344
Anton Bochkovskiy <unundertale1@ukr.net>
2024-09-07 21:36:01 +0300

    render title, date, and amount, use local styles

--- src/components/ExpensesContainer/components/ExpenseCard/index.tsx
+++ src/components/ExpensesContainer/components/ExpenseCard/index.tsx
@@ -1,17 +1,30 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 
 import { Expense } from '../../types'
+import { styles } from './styles'
 
 interface ExpenseCardProps {
   data: Expense
 }
 
 const ExpenseCard: FC<ExpenseCardProps> = ({ data }) => {
+  const { title, date, amount } = data
+
+  const ISODate = date.toISOString()
+
   return (
-    <View>
-      <Text>{data.title}</Text>
-    </View>
+    <Pressable>
+      <View style={styles.container}>
+        <View>
+          <Text style={[styles.text, styles.title]}>{title}</Text>
+          <Text style={styles.text}>{ISODate}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{amount}</Text>
+        </View>
+      </View>
+    </Pressable>
   )
 }
 
